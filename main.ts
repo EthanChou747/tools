6f727126bfd556c692ae8b887ae795ce8cd3a750
EthanChou747 <ccchou@apps.ntpc.edu.tw>
2024-12-05 14:15:22 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-//% weight=0 color=#3CB371 icon="\uf04e" block="Tools"
+//% weight=0 color=#222222 icon="\uf04e" block="Tools"
 namespace tools {
     /**
     * 計算長方形面積，並回傳
     */
     //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
+    //% blockGap=2 weight=1 blockExternalInputs=true
     export function areaOfRectangle(length: number, width:number): number {
         return length*width
     }
@@ -12,7 +12,7 @@ namespace tools {
     * 計算長方形面積，不回傳，只顯示在LED
     */
     //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
+    //% blockGap=10 weight=0
     export function ledOfRectangle(length: number, width:number): void {
         basic.showNumber(length*width)
     }
